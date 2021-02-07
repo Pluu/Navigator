210b63ec97dfa6f94e05a926572693ed37817ffd
pluulove <pluulove@gmail.com>
2021-02-08 01:53:19 +0900

    불필요 에러 제거

--- Navigator/src/main/java/com/pluu/navigator/exception/Exception.kt
+++ Navigator/src/main/java/com/pluu/navigator/exception/Exception.kt
@@ -3,8 +3,3 @@ package com.pluu.navigator.exception
 class MissingRouteThrowable(
     routeName: String
 ) : Throwable("no route found for $routeName")
-
-class MissingThrowable(
-    type: String,
-    routeName: String
-) : Throwable("no $type found for $routeName")
