4fad21fbade0f607db5a698460f39869918d562d
pluulove <pluulove@gmail.com>
2021-02-12 17:50:30 +0900

    Update Info

--- Navigator/src/main/java/com/pluu/navigator/deeplink/DeepLinkMatchResult.kt
+++ Navigator/src/main/java/com/pluu/navigator/deeplink/DeepLinkMatchResult.kt
@@ -3,11 +3,11 @@ package com.pluu.navigator.deeplink
 import com.pluu.navigator.Destination
 
 /**
- * 매칭된 결과를 전달하는 용도의 Wrapper Class
+ * Wrapper class with matching result information according to request
  *
- * @property request 요청한 DeepLink 정보
- * @property destination 매칭된 Destination 정보
- * @property args {type} 형태로 정의된 QueryString Parameter 정보
+ * @property request Requested DeepLink information
+ * @property destination Matched destination information
+ * @property args Parameter information defined in {type} format
  */
 class DeepLinkMatchResult(
     val request: DeepLinkRequest,
