b2357f576d6a5aa0aa6ba543d977f0ffac6f6d7e
pluulove <pluulove@gmail.com>
2021-02-15 23:07:23 +0900

    Update Test

--- Navigator/src/test/java/com/pluu/navigator/deeplink/NavDeepLinkTest.kt
+++ Navigator/src/test/java/com/pluu/navigator/deeplink/NavDeepLinkTest.kt
@@ -21,20 +21,6 @@ class NavDeepLinkTest {
         assertFalse(navDeepLink.match("pluu://test/test?type=1"))
     }
 
-    private fun NavDeepLink.easyMatchingArguments(
-        link: String,
-        isNonNull: Boolean,
-        argSize: Int = 0
-    ) {
-        val result = matchingArguments(link)
-        if (isNonNull) {
-            requireNotNull(result)
-            assertEquals(argSize, result.size)
-        } else {
-            assertNull(result)
-        }
-    }
-
     @Test
     fun matchingArguments_Path() {
         val navDeepLink = NavDeepLink("pluu://test/{id}")
@@ -45,8 +31,12 @@ class NavDeepLinkTest {
         navDeepLink.easyMatchingArguments("pluu://test?type=abc", false)
 
         // Matching
-        navDeepLink.easyMatchingArguments("pluu://test/1", true, 1)
-        navDeepLink.easyMatchingArguments("pluu://test/1?type=abc", true, 1)
+        navDeepLink.easyMatchingArguments("pluu://test/1", true) {
+            assertEntry(mapOf<String, Any>("id" to "1"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test/1?type=abc", true) {
+            assertEntry(mapOf<String, Any>("id" to "1"), it)
+        }
     }
 
     @Test
@@ -57,8 +47,12 @@ class NavDeepLinkTest {
         navDeepLink.easyMatchingArguments("blablabla", false)
 
         // Matching
-        navDeepLink.easyMatchingArguments("pluu://test", true, 0)
-        navDeepLink.easyMatchingArguments("pluu://test?type=1", true, 0)
+        navDeepLink.easyMatchingArguments("pluu://test", true) {
+            assertEntry(emptyMap(), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?type=1", true) {
+            assertEntry(emptyMap(), it)
+        }
     }
 
     @Test
@@ -69,10 +63,18 @@ class NavDeepLinkTest {
         navDeepLink.easyMatchingArguments("blablabla", false)
 
         // Matching
-        navDeepLink.easyMatchingArguments("pluu://test", true, 0)
-        navDeepLink.easyMatchingArguments("pluu://test?none=1", true, 0)
-        navDeepLink.easyMatchingArguments("pluu://test?type=abc", true, 1)
-        navDeepLink.easyMatchingArguments("pluu://test?abc=type&type=abc", true, 1)
+        navDeepLink.easyMatchingArguments("pluu://test", true) {
+            assertEntry(emptyMap(), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?none=1", true) {
+            assertEntry(emptyMap(), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?type=abc", true) {
+            assertEntry(mapOf<String, Any>("type" to "abc"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?abc=type&type=abc", true) {
+            assertEntry(mapOf<String, Any>("type" to "abc"), it)
+        }
     }
 
     @Test
@@ -83,11 +85,72 @@ class NavDeepLinkTest {
         navDeepLink.easyMatchingArguments("blablabla", false)
 
         // Matching
-        navDeepLink.easyMatchingArguments("pluu://test", true, 0)
-        navDeepLink.easyMatchingArguments("pluu://test?none=1", true, 0)
-        navDeepLink.easyMatchingArguments("pluu://test?arg1=abc", true, 1)
-        navDeepLink.easyMatchingArguments("pluu://test?arg2=abc", true, 1)
-        navDeepLink.easyMatchingArguments("pluu://test?arg1=abc&arg2=abc", true, 2)
-        navDeepLink.easyMatchingArguments("pluu://test?arg2=abc&arg1=abc", true, 2)
+        navDeepLink.easyMatchingArguments("pluu://test", true) {
+            assertEntry(emptyMap(), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?none=1", true) {
+            assertEntry(emptyMap(), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?arg1=abc", true) {
+            assertEntry(mapOf<String, Any>("arg1" to "abc"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?arg2=abc", true) {
+            assertEntry(mapOf<String, Any>("arg2" to "abc"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?arg1=abc&arg2=def", true) {
+            assertEntry(mapOf<String, Any>("arg1" to "abc", "arg2" to "def"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test?arg2=abc&arg1=def", true) {
+            assertEntry(mapOf<String, Any>("arg2" to "abc", "arg1" to "def"), it)
+        }
+    }
+
+    @Test
+    fun matchingArguments_path_Multiple_QueryString() {
+        val navDeepLink = NavDeepLink("pluu://test/{id}?arg1={arg1}&arg2={arg2}")
+
+        // Not Matching
+        navDeepLink.easyMatchingArguments("blablabla", false)
+
+        // Matching
+        navDeepLink.easyMatchingArguments("pluu://test", false)
+        navDeepLink.easyMatchingArguments("pluu://test?none=1", false)
+        navDeepLink.easyMatchingArguments("pluu://test/qwe", true) {
+            assertEntry(mapOf<String, Any>("id" to "qwe"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test/qwe?arg1=abc", true) {
+            assertEntry(mapOf<String, Any>("id" to "qwe", "arg1" to "abc"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test/qwe?arg2=abc", true) {
+            assertEntry(mapOf<String, Any>("id" to "qwe", "arg2" to "abc"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test/qwe?arg1=abc&arg2=def", true) {
+            assertEntry(mapOf<String, Any>("id" to "qwe", "arg1" to "abc", "arg2" to "def"), it)
+        }
+        navDeepLink.easyMatchingArguments("pluu://test/qwe?arg2=abc&arg1=def", true) {
+            assertEntry(mapOf<String, Any>("id" to "qwe", "arg2" to "abc", "arg1" to "def"), it)
+        }
+    }
+
+    private fun NavDeepLink.easyMatchingArguments(
+        link: String,
+        isMatched: Boolean,
+        argsCheck: (Map<String, Any>) -> Unit = {}
+    ) {
+        val result = matchingArguments(link)
+        if (isMatched) {
+            requireNotNull(result)
+            argsCheck(result)
+        } else {
+            assertNull(result)
+        }
+    }
+
+    private fun assertEntry(expected: Map<String, Any>, actual: Map<String, Any>) {
+        assertEquals(expected.size, actual.size)
+        expected.forEach { (key, value) ->
+            assertTrue(actual.containsKey(key))
+            assertEquals(actual[key], value)
+        }
     }
 }
