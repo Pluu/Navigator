47acabd05425ee6cc04bece20217dc9ba70033b2
pluulove <pluulove@gmail.com>
2021-03-10 02:02:36 +0900

    Binding, Default Test SDK

--- Navigator/src/test/java/com/pluu/navigator/deeplink/NavDeepLinkTest.kt
+++ Navigator/src/test/java/com/pluu/navigator/deeplink/NavDeepLinkTest.kt
@@ -4,10 +4,8 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Config.OLDEST_SDK])
 class NavDeepLinkTest {
 
     @Test
