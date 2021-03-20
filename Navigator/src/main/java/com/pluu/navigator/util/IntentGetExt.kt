5143e515e1724e92014020e045ec34c2574acbdc
pluulove <pluulove@gmail.com>
2021-03-21 00:28:23 +0900

    Update Direction Param Extension

--- Navigator/src/main/java/com/pluu/navigator/util/IntentGetExt.kt
+++ Navigator/src/main/java/com/pluu/navigator/util/IntentGetExt.kt
@@ -1,28 +1,33 @@
 package com.pluu.navigator.util
 
 import android.app.Activity
+import android.os.Bundle
 import com.pluu.navigator.DIRECTION_PARAMS_KEY
 import kotlin.properties.ReadOnlyProperty
 
+fun <T> Bundle.findDirectionParam() = get(DIRECTION_PARAMS_KEY) as T
+
+fun <T> Bundle.optionalDirectionParam() = get(DIRECTION_PARAMS_KEY) as? T
+
 fun <T> bindExtra() = extraDelegate {
-    it.intent.extras!!.get(DIRECTION_PARAMS_KEY) as T
+    it.intent.extras!!.findDirectionParam<T>()
 }
 
 fun <T> optionalBindExtra() = extraDelegate {
-    it.intent.extras?.get(DIRECTION_PARAMS_KEY) as? T
+    it.intent.extras?.optionalDirectionParam<T>()
 }
 
 fun <T, R> bindExtra(
     getter: (param: T) -> R
 ) = extraDelegate {
-    val args: T = it.intent.extras!!.get(DIRECTION_PARAMS_KEY) as T
+    val args: T = it.intent.extras!!.findDirectionParam()
     getter(args)
 }
 
 fun <T, R> optionalBindExtra(
     getter: (param: T?) -> R?
 ) = extraDelegate {
-    val args = it.intent.extras?.get(DIRECTION_PARAMS_KEY) as? T
+    val args = it.intent.extras?.optionalDirectionParam<T>()
     getter(args)
 }
 
